fa79979ee4f0268cce816c3ce179ae050deccf4e
craankism <saschaschwellnus@gmail.com>
2026-08-25 11:05:09 +0200

    chore: group modal styling

---
+++ src/constants/modalStyle.ts
@@ -0,0 +1,11 @@
+export const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 350,
+  bgcolor: "#F5DCC6",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
